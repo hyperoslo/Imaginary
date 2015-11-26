f3f853e5a68f359607b1d5129f03966849dd1dfc
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-26 11:32:44 +0100

    Adds the basic configuration to show the controller

--- Demo/ImaginaryDemo/ImaginaryDemo/AppDelegate.swift
+++ Demo/ImaginaryDemo/ImaginaryDemo/AppDelegate.swift
@@ -3,9 +3,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  lazy var viewController: ViewController = ViewController()
+
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow()
+    window?.rootViewController = viewController
+    window?.makeKeyAndVisible()
+
     return true
   }
 }
--- Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
+++ Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
@@ -4,6 +4,8 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    view.backgroundColor = UIColor.whiteColor()
   }
 }
 
