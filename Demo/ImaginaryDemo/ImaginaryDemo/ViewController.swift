dcb3c45804eba5a77e876a0063588fe807d1f9a5
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-26 12:38:09 +0100

    Adds another cell

--- Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
+++ Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -9,3 +9,24 @@ class ViewController: UIViewController {
   }
 }
 
+// MARK: - TableView Delegate
+
+extension ViewController {
+
+  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    return 50
+  }
+}
+
+// MARK: - TableView DataSource
+
+extension ViewController {
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
