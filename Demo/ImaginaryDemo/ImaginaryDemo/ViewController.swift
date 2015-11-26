6c7aa53d0df2154c4eafe31967c77bcffe2ae6ad
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-26 11:18:40 +0100

    Deletes some unneded stuff in the files

--- Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
+++ Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
@@ -1,25 +1,9 @@
-//
-//  ViewController.swift
-//  ImaginaryDemo
-//
-//  Created by Ramon Gilabert Llop on 11/26/15.
-//  Copyright © 2015 Ramon Gilabert Llop. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
