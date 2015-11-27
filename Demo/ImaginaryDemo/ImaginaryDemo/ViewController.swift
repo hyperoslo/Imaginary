f371971f17a98cb5ce6e1c736d8dbcb345d0e05c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-27 10:39:48 +0100

    Adds some configuration to download the images

--- Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
+++ Demo/ImaginaryDemo/ImaginaryDemo/ViewController.swift
@@ -1,6 +1,28 @@
 import UIKit
+import Fakery
 
 class ViewController: UITableViewController {
+
+  struct ImageDimensions {
+    static let width = 500
+    static let height = 500
+  }
+
+  lazy var imaginaryArray: [NSURL] = {
+    let faker = Faker()
+    var array = [NSURL]()
+
+    for i in 0..<20 {
+      if let imageURL = NSURL(
+        string: "http://lorempixel.com/\(ImageDimensions.width)/\(ImageDimensions.height)/?type=attachment&id=\(i)\(50)") {
+        array.append(imageURL)
+      }
+    }
+
+    return array
+    }()
+
+  // MARK: - View lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -12,6 +34,12 @@ class ViewController: UITableViewController {
     tableView.delegate = self
     tableView.dataSource = self
     tableView.separatorStyle = .None
+  }
+
+  // MARK: - Setup tableView
+
+  func setupTableView() {
+
   }
 }
 
@@ -29,7 +57,7 @@ extension ViewController {
 extension ViewController {
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return imaginaryArray.count
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
