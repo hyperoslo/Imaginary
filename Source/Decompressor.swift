40d9faf32525a6cfc64a4b4d2c37db07d487867f
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-11-27 13:37:02 +0100

    Adds file

--- Source/Decompressor.swift
+++ Source/Decompressor.swift
@@ -1,13 +1,23 @@
-//
-//  Decompressor.swift
-//  Pods
-//
-//  Created by Ramon Gilabert Llop on 11/27/15.
-//
-//
-
 import UIKit
 
-class Decompressor: NSObject {
+public struct Decompressor {
 
+  public func decompress(data: NSData, scale: CGFloat = 1, completion: (image: UIImage) -> Void) {
+    autoreleasepool({
+      dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), {
+        guard let image = UIImage(data: data) else { completion(image: UIImage()); return }
+
+        let reference = image.CGImage
+
+        if CGImageGetAlphaInfo(reference) != .None { dispatch_async(dispatch_get_main_queue(), { completion(image: image) }) }
+
+        UIGraphicsBeginImageContextWithOptions(image.size, true, scale)
+        image.drawInRect(CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        let blendedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        dispatch_async(dispatch_get_main_queue(), { completion(image: blendedImage) })
+      })
+    })
+  }
 }
