362f334194d8edf9deb9486facc4074469741a58
Vadym Markov <markov.vadym@gmail.com>
2015-11-30 13:34:31 +0100

    Custom cache config

--- Source/Imaginary.swift
+++ Source/Imaginary.swift
@@ -2,6 +2,12 @@ import Cache
 
 public var imageCache: Cache<UIImage> {
   struct Static {
+    static let config = Config(
+      frontKind: .Memory,
+      backKind: .Disk,
+      expiry: .Date(NSDate().dateByAddingTimeInterval(60 * 60 * 24 * 3)),
+      maxSize: 0)
+
     static let cache = Cache<UIImage>(name: "Imaginary")
   }
 
