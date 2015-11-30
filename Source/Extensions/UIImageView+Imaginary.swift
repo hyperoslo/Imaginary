5e9cb8557b02ae5f84d48af16b6139aa14bd48b4
Vadym Markov <markov.vadym@gmail.com>
2015-11-30 13:28:31 +0100

    Set remote image

--- Source/Extensions/UIImageView+Imaginary.swift
+++ Source/Extensions/UIImageView+Imaginary.swift
@@ -1,9 +1,34 @@
-//
-//  UIImageView+Imaginary.swift
-//  Imaginary
-//
-//  Created by Vadym Markov on 30/11/15.
-//  Copyright © 2015 Example. All rights reserved.
-//
+import UIKit
 
-import Foundation
+extension UIImageView {
+
+  func setRemoteImage(URL: NSURL, placeholder : UIImage? = nil) {
+    let key = URL.absoluteString
+    image = placeholder
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+      imageCache.object(key) { [weak self] object in
+        guard let weakSelf = self else { return }
+
+        if let object = object {
+          weakSelf.image = object
+          return
+        }
+
+        let fetcher = ImageFetcher(URL: URL)
+
+        fetcher.start { [weak self] result in
+          guard let weakSelf = self else { return }
+
+          switch result {
+          case let .Success(image):
+            weakSelf.image = image
+            imageCache.add(key, object: image)
+          default:
+            break
+          }
+        }
+      }
+    }
+  }
+}
