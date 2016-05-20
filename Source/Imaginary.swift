aa59864885097ada1d2c1f2677a7f60e9ca58477
Christoffer Winterkvist <christoffer@hyper.no>
2016-05-20 15:31:55 +0200

    Add transitionClosure

--- Source/Imaginary.swift
+++ Source/Imaginary.swift
@@ -6,6 +6,19 @@ public struct Imaginary {
   public static var preConfigure: ((imageView: UIImageView) -> Void)? = { imageView in
     imageView.layer.opacity = 0.0
   }
+  
+  public static var transitionClosure: ((imageView: UIImageView, image: UIImage) -> Void) = { imageView, newImage in
+    guard let oldImage = imageView.image else {
+      imageView.image = newImage
+      return
+    }
+
+    let animation = CABasicAnimation(keyPath: "contents")
+    animation.duration = 0.25
+    animation.fromValue = oldImage.CGImage
+    animation.toValue = newImage.CGImage
+    imageView.layer.addAnimation(animation, forKey: "transitionAnimation")
+    imageView.image = newImage
   }
 
   public static var postConfigure: ((imageView: UIImageView) -> Void)? = { imageView in
