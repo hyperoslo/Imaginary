bb4a8f105fc1ff426717435d26976b5569b200bf
Vadym Markov <markov.vadym@gmail.com>
2016-09-02 15:09:12 +0200

    Tint color effect

--- Sources/iOS/Drawers/Drawers.swift
+++ Sources/iOS/Drawers/Drawers.swift
@@ -1,9 +1,32 @@
-//
-//  Drawers.swift
-//  Imaginary
-//
-//  Created by Vadym Markov on 02/09/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public protocol ImageDrawer {
+  func draw(image: UIImage, context: CGContext, rect: CGRect)
+}
+
+// MARK: - Tint effect
+
+public struct TintDrawer: ImageDrawer {
+
+  public let tintColor: UIColor
+
+  public init(tintColor: UIColor) {
+    self.tintColor = tintColor
+  }
+
+  public func draw(image: UIImage, context: CGContext, rect: CGRect) {
+    CGContextSetBlendMode(context, .Normal)
+    UIColor.blackColor().setFill()
+    CGContextFillRect(context, rect)
+
+    CGContextSetBlendMode(context, .Normal)
+    CGContextDrawImage(context, rect, image.CGImage)
+
+    CGContextSetBlendMode(context, .Color)
+    tintColor.setFill()
+    CGContextFillRect(context, rect)
+
+    CGContextSetBlendMode(context, .DestinationIn)
+    CGContextDrawImage(context, rect, image.CGImage)
+  }
+}
