ba3524dcf9c9fb67196f7c05fed334c0e302a0aa
Vadym Markov <markov.vadym@gmail.com>
2016-09-07 17:05:48 +0200

    Fix color space in decompression

--- Sources/iOS/Decompressor.swift
+++ Sources/iOS/Decompressor.swift
@@ -11,7 +11,18 @@ struct Decompressor {
 
     UIGraphicsBeginImageContextWithOptions(image.size, true, scale)
 
-    let context = UIGraphicsGetCurrentContext()
+    let bitmapInfo = CGImageGetBitmapInfo(reference)
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+
+    let pixelWidth = Int(ceil(image.size.width * image.scale))
+    let pixelHeight = Int(ceil(image.size.height * image.scale))
+
+    guard let context = CGBitmapContextCreate(
+      nil, pixelWidth, pixelHeight,
+      CGImageGetBitsPerComponent(reference),
+      0, colorSpace, bitmapInfo.rawValue)
+      else { return image }
+
     CGContextSetRGBFillColor(context, 1, 1, 1, 1)
     CGContextFillRect(context, CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
     CGContextSaveGState(context)
