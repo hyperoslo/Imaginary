30f1be6925f238437ef802e586f01010d4a5e2bd
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-09-07 17:13:35 +0200

    Fixes the decompressor causing blank image

--- Sources/iOS/Decompressor.swift
+++ Sources/iOS/Decompressor.swift
@@ -4,33 +4,6 @@ struct Decompressor {
 
   static func decompress(data: NSData, scale: CGFloat = 1) -> Image {
     guard let image = Image(data: data) else { return Image() }
-
-    let reference = image.CGImage
-
-    if CGImageGetAlphaInfo(reference) != .None { return image }
-
-    UIGraphicsBeginImageContextWithOptions(image.size, true, scale)
-
-    let bitmapInfo = CGImageGetBitmapInfo(reference)
-    let colorSpace = CGColorSpaceCreateDeviceRGB()
-
-    let pixelWidth = Int(ceil(image.size.width * image.scale))
-    let pixelHeight = Int(ceil(image.size.height * image.scale))
-
-    guard let context = CGBitmapContextCreate(
-      nil, pixelWidth, pixelHeight,
-      CGImageGetBitsPerComponent(reference),
-      0, colorSpace, bitmapInfo.rawValue)
-      else { return image }
-
-    CGContextSetRGBFillColor(context, 1, 1, 1, 1)
-    CGContextFillRect(context, CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-    CGContextSaveGState(context)
-
-    let blendedImage = UIGraphicsGetImageFromCurrentImageContext()
-
-    UIGraphicsEndImageContext()
-
-    return blendedImage
+    return image
   }
 }
