94a678bb0353a8c5c71965c467be80fa0c3d83fc
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-09-28 15:58:08 +0200

    Updates the demoes

--- Sources/Shared/Image+Cache.swift
+++ Sources/Shared/Image+Cache.swift
@@ -6,6 +6,6 @@ extension Image {
   public typealias CacheType = Image
 
   public static func decode(_ data: Data) -> CacheType? {
-    return Decompressor.decompress(data)
+    return Decompressor.decompress(data: data as NSData)
   }
 }
