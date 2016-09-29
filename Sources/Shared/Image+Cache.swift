b8780d8f5037ea49050bdca18087b9c92f53f502
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-09-29 16:36:16 +0200

    Fixes a small warning

--- Sources/Shared/Image+Cache.swift
+++ Sources/Shared/Image+Cache.swift
@@ -6,6 +6,6 @@ extension Image {
   public typealias CacheType = Image
 
   public static func decode(_ data: Data) -> CacheType? {
-    return Decompressor.decompress(data: data as NSData)
+    return Decompressor.decompress(data as Data)
   }
 }
