d50bdf04773b44147a8765e9c4cf33e75c13d297
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-06-07 14:47:50 +0200

    Fix typo when migrating labels

--- Sources/Shared/Configuration.swift
+++ Sources/Shared/Configuration.swift
@@ -8,8 +8,8 @@ public struct Configuration {
   public static var imageCache: SpecializedCache<Image> = {
     let config = Config(
         expiry: Expiry.date(NSDate().addingTimeInterval(60 * 60 * 24 * 3) as Date),
-        memoryCountLimit: 0,
-        maxDiskSize: 10)
+        memoryCountLimit: 10,
+        maxDiskSize: 0)
 
     return SpecializedCache<Image>(name: "Imaginary", config: config)
   }()
