adcb8a82906cc6031d470892467813a8aa37c6fc
Khoa Pham <onmyway133@gmail.com>
2017-09-18 16:17:20 +0200

    Declare storage

--- Sources/Shared/Configuration.swift
+++ Sources/Shared/Configuration.swift
@@ -3,12 +3,11 @@ import Cache
 
 public struct Configuration {
   public static var bytesLoaded: Int = 0
-  public static var imageCache: SpecializedCache<Image> = {
-    let config = Config(
-        expiry: Expiry.date(NSDate().addingTimeInterval(60 * 60 * 24 * 3) as Date),
-        memoryCountLimit: 10,
-        maxDiskSize: 0)
 
-    return SpecializedCache<Image>(name: "Imaginary", config: config)
+  public static var imageStorage: Storage? = {
+    let diskConfig = DiskConfig(name: "Imaginary", expiry: .date(Date().addingTimeInterval(60 * 60 * 24 * 3)))
+    let memoryConfig = MemoryConfig(countLimit: 10, totalCostLimit: 0)
+
+    return try? Storage(diskConfig: diskConfig, memoryConfig: memoryConfig)
   }()
 }
