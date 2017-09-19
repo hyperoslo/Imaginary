0b7fa2fb1b42ca23dae4a52857bb51d6aae6083d
Khoa Pham <onmyway133@gmail.com>
2017-09-19 09:07:30 +0200

    Remove Image+Cache

--- Sources/Shared/Image+Cache.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Cache
-
-extension Image {
-  public typealias CacheType = Image
-  public static func decode(_ data: Data) -> CacheType? {
-    return Decompressor.decompress(data as Data)
-  }
-}
