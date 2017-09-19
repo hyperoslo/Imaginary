9902a611c632893cbefb7f5712f3fa1fbc12d3a9
Khoa Pham <onmyway133@gmail.com>
2017-09-19 11:00:16 +0200

    Fix test target

--- ImaginaryTests-iOS/ImaginaryTests_iOS.swift
+++ ImaginaryTests-iOS/ImaginaryTests_iOS.swift
@@ -13,15 +13,6 @@ class ImaginaryTests_iOS: XCTestCase {
   }
 
   func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssertEqual(1, 1)
   }
-
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
-  }
-
 }
