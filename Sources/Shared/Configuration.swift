e1314cca1add7dd946d9742945d147dd4f1f9867
Khoa Pham <onmyway133@gmail.com>
2017-09-19 09:51:00 +0200

    Move things to Configuration

--- Sources/Shared/Configuration.swift
+++ Sources/Shared/Configuration.swift
@@ -1,13 +1,69 @@
 import Foundation
 import Cache
 
+/// Configuration
 public struct Configuration {
+
+  /// Track how many bytes have been used for downloading
   public static var bytesLoaded: Int = 0
 
-  public static var imageStorage: Storage? = {
+  /// The image storage, used for caching images in memory and disk
+  public var imageStorage: Storage? = {
     let diskConfig = DiskConfig(name: "Imaginary", expiry: .date(Date().addingTimeInterval(60 * 60 * 24 * 3)))
     let memoryConfig = MemoryConfig(countLimit: 10, totalCostLimit: 0)
 
     return try? Storage(diskConfig: diskConfig, memoryConfig: memoryConfig)
   }()
+
+  /// Toggle whether image should be fetched from cache first
+  public var usesCache: Bool = true
+
+  /// Pre process fetched image
+  public var preprocess: ((Image) -> Image) = {
+    return $0
+  }
+
+  /// Pre configure imageView before setting image
+  public var preConfigure: ((ImageView) -> Void)? = { imageView in
+    #if os(iOS) || os(tvOS)
+      imageView.layer.opacity = 0.0
+    #endif
+  }
+
+  /// Animation when new image is set
+  public var transitionClosure: ((ImageView, Image) -> Void) = { imageView, newImage in
+    #if os(iOS) || os(tvOS)
+      guard let oldImage = imageView.image else {
+        imageView.image = newImage
+        return
+      }
+
+      let animation = CABasicAnimation(keyPath: "contents")
+      animation.duration = 0.25
+      animation.fromValue = oldImage.cgImage
+      animation.toValue = newImage.cgImage
+      imageView.layer.add(animation, forKey: "transitionAnimation")
+      imageView.image = newImage
+    #endif
+  }
+
+  /// Post configure imageView after setting image
+  public var postConfigure: ((ImageView) -> Void)? = { imageView in
+    #if os(iOS) || os(tvOS)
+      let animation = CABasicAnimation(keyPath: "opacity")
+      animation.fromValue = imageView.layer.opacity
+      animation.toValue = 1.0
+      imageView.layer.add(animation, forKey: "fadeAnimation")
+      imageView.layer.opacity = 1.0
+    #endif
+  }
+
+  /// Track the url for error
+  public var track: ((URL?, Error?, Int) -> Void)?
+
+  /// Initialization
+  public init() {}
+
+  /// The default configuration applied to every image request
+  public static var `default` = Configuration()
 }
