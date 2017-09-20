5eb75e71e3f8fed6b726bf114bf441882f8b3354
Khoa Pham <onmyway133@gmail.com>
2017-09-20 10:29:10 +0200

    Add TintImageProcessor

--- Sources/iOS/TintImageProcessor.swift
+++ Sources/iOS/TintImageProcessor.swift
@@ -1,9 +1,57 @@
-//
-//  TintImageProcessor.swift
-//  Imaginary-iOS
-//
-//  Created by Khoa Pham on 20.09.2017.
-//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+/// Apply tint color to image
+public class TintImageProcessor: ImageProcessing {
+  let tintColor: UIColor
+
+  public init(tintColor: UIColor) {
+    self.tintColor = tintColor
+  }
+
+  public func process(image: Image) -> Image {
+    UIGraphicsBeginImageContextWithOptions(image.size, false, image.scale)
+
+    guard let context = UIGraphicsGetCurrentContext() else {
+      return image
+    }
+
+    let rect = CGRect(x: 0, y: 0,
+                      width: image.size.width,
+                      height: image.size.height)
+
+    context.translateBy(x: 0, y: image.size.height)
+    context.scaleBy(x: 1.0, y: -1.0)
+
+    apply(tintColor: tintColor,
+          onto: image,
+          context: context, rect: rect)
+
+    let processedImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    return processedImage ?? image
+  }
+
+  private func apply(tintColor: UIColor,
+                     onto image: UIImage,
+                     context: CGContext,
+                     rect: CGRect) {
+    guard let cgImage = image.cgImage else {
+      return
+    }
+
+    context.setBlendMode(.normal)
+    UIColor.black.setFill()
+    context.fill(rect)
+
+    context.setBlendMode(.normal)
+    context.draw(cgImage, in: rect)
+
+    context.setBlendMode(.color)
+    tintColor.setFill()
+    context.fill(rect)
+
+    context.setBlendMode(.destinationIn)
+    context.draw(cgImage, in: rect)
+  }
+}
