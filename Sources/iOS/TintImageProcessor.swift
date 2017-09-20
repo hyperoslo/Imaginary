80db48cf0da5d45f48535052f8153ae947a44354
Khoa Pham <onmyway133@gmail.com>
2017-09-20 10:29:01 +0200

    Remove Drawer

---
+++ Sources/iOS/TintImageProcessor.swift
@@ -0,0 +1,9 @@
+//
+//  TintImageProcessor.swift
+//  Imaginary-iOS
+//
+//  Created by Khoa Pham on 20.09.2017.
+//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
