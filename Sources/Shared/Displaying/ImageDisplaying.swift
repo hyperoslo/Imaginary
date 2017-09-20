f0dd3daa5a8f7fd2267c1bb36cc7be6e43314646
Khoa Pham <onmyway133@gmail.com>
2017-09-20 09:56:22 +0200

    Add ImageDisplaying and ImageProcessing

---
+++ Sources/Shared/Displaying/ImageDisplaying.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDisplaying.swift
+//  Imaginary
+//
+//  Created by Khoa Pham on 20.09.2017.
+//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Shared/Processing/ImageProcessing.swift
@@ -0,0 +1,9 @@
+//
+//  ImageProcessing.swift
+//  Imaginary
+//
+//  Created by Khoa Pham on 20.09.2017.
+//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
