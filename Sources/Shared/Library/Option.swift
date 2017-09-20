c34c4383d8befe53a28f47e4ed7f7fb50acc6676
Khoa Pham <onmyway133@gmail.com>
2017-09-20 09:56:29 +0200

    Add Option

--- Sources/Shared/Library/Option.swift
+++ Sources/Shared/Library/Option.swift
@@ -1,7 +1,17 @@
 import Foundation
+import Cache
 
-
-
+/// Option applied when fetching image
 public struct Option {
 
+  /// Pre process downloaded image before it gets applied to UI
+  public var imagePreprocessor: ImageProcessing?
+
+  /// To apply transition or custom animation when display image
+  public var imageDisplayer: ImageDisplaying?
+
+  /// The image storage, defaults to Configuration.imageStorage
+  public var imageStorage: Storage = Configuration.imageStorage
+
+  public init() {}
 }
