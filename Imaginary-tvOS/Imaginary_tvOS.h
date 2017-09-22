27236b534efb869e3b93fb015928a00010a3c640
Khoa Pham <onmyway133@gmail.com>
2017-09-22 15:05:36 +0200

    Add tvOS support

---
+++ Imaginary-tvOS/Imaginary_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Imaginary_tvOS.h
+//  Imaginary-tvOS
+//
+//  Created by Khoa Pham on 22.09.2017.
+//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Imaginary_tvOS.
+FOUNDATION_EXPORT double Imaginary_tvOSVersionNumber;
+
+//! Project version string for Imaginary_tvOS.
+FOUNDATION_EXPORT const unsigned char Imaginary_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Imaginary_tvOS/PublicHeader.h>
+
+
